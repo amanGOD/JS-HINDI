5769893daf8cae8bf60e2fe155e951715a971f9c
amanGOD <amangupta1622k3@gmail.com>
2024-11-22 12:38:02 +0000

    container config done

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,9 @@
+const accountId =1234
+let accountEmail = "amangupta1622k3@gmail.com"
+var accountPassword ="1234"
+accountCity ="Jaipuur"
+//accountId = 2
+accountEmail ="bhbj@gmail.com"
+accountPassword ="2424352"
+accountCity =" bengal"
+console.table([accountId,accountEmail,accountPassword,accountCity])
