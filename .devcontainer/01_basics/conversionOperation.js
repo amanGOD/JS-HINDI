88d09d3182c570df1532bf68e8d1e743a2d03f15
amanGOD <amangupta1622k3@gmail.com>
2024-11-22 17:56:23 +0000

    container config done

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,13 @@
+let score = true
+//const {score} =requestAnimationFrame.body
+//console.log(typeof score);
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+ let isloggedIn=1
+ let booleanIsLoggedIn = Boolean(isloggedIn)
+ console.log(booleanIsLoggedIn);
+ 
+ 
+ 
+ 
--- .devcontainer/01_basics/datatype.js
+++ .devcontainer/01_basics/datatype.js
@@ -1,3 +1,6 @@
 "use strict";
 
 //alert(3+3)
+let name = "aman"
+let age =23
+let isloggedIn = false
